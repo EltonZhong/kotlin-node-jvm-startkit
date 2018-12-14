81d2bff7d701c3cacf01563f121b3c508ea881a6
Elton Zhong <elton.zhong@lmxmn150.rcoffice.ringcentral.com>
2018-12-14 10:21:33 +0800

    Output to node_modules

--- src/commonMain/kotlin/coroutines/main.kt
+++ src/commonMain/kotlin/coroutines/main.kt
@@ -19,23 +19,6 @@ fun getDeferred(): Deferred<Int> {
 }
 
 @ExperimentalCoroutinesApi
-suspend fun main(args: Array<String>) {
-    GlobalScope.launch {
-        println("something after0 ")
-        delay(1000)
-        println("something after1 ")
-        delay(1000)
-        println("something after2 ")
-    }
-
-    delay(1000)
-    println("A")
-    delay(3000)
-    println("B")
-    println(platform())
-    println(getInt())
-    println(getDeferred().await())
-    get()
-}
+expect suspend fun main(args: Array<String>)
 
 suspend expect fun platform(): String
--- src/jvmMain/kotlin/coroutines/platform.kt
+++ src/jvmMain/kotlin/coroutines/platform.kt
@@ -1,8 +1,13 @@
 package coroutines
 
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.delay
 
 actual suspend fun platform(): String {
     delay(100)
     return "jvm"
+}
+
+@ExperimentalCoroutinesApi
+actual suspend fun main(args: Array<String>) {
 }
